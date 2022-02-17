72579fac3a9ee37703a2f5295cb5eec79f152361
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-17 20:56:02 +0530

    ADDED:greeting controller to return JSON for different HTTP methods

---
+++ src/main/java/com/bridgelabz/greetingapplicationservice/GreetingapplicationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingapplicationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingapplicationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/greetingapplicationservice/GreetingapplicationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingapplicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingapplicationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
