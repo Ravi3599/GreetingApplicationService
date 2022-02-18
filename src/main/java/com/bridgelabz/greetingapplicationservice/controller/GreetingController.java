1779b176a2c4375a424c037a1e853f9ba9e277d3
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-18 13:43:40 +0530

    UPDATED: ability to delete greeting message

--- src/main/java/com/bridgelabz/greetingapplicationservice/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapplicationservice/controller/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,7 +65,11 @@ public class GreetingController {
 	}
 	@PutMapping("/editGreetingById/{id}")
 	public ResponseEntity<Greeting> editGreeting(@RequestParam String content,@PathVariable Integer id){
-		return new ResponseEntity<Greeting>(greetingService.editData(id,content),HttpStatus.OK);
+		return new ResponseEntity<Greeting>(greetingService.editDataById(id,content),HttpStatus.OK);
+	}
+	@DeleteMapping("/deleteGreetingById/{id}")
+	public ResponseEntity<String> deleteGreetingById(@PathVariable Integer id){
+		return new ResponseEntity<String>(greetingService.deleteDataById(id),HttpStatus.OK);
 	}
 }
 
--- src/main/java/com/bridgelabz/greetingapplicationservice/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapplicationservice/service/GreetingService.java
@@ -38,9 +38,13 @@ public class GreetingService {
 		List<Greeting>list=repo.findAll();
 		return list;
 	}
-	public Greeting editData(Integer id,String content) {
+	public Greeting editDataById(Integer id,String content) {
 		Greeting newGreeting= new Greeting(id,String.format(template,content));
 		repo.save(newGreeting);
 		return newGreeting;
+	}
+	public String deleteDataById(Integer id) {
+		repo.deleteById(id);
+		return "Greeting message having id "+id+", got deleted";
 	}
 }
